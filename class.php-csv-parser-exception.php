28aac9857058ce0ee9e5863e53d0ce06d2869d22
okeke emmanuel <emmanix2002@gmail.com>
2013-06-27 12:45:42 +0100

    some additional methods defined

---
+++ class.php-csv-parser-exception.php
@@ -0,0 +1,103 @@
+<?php
+	namespace PhpCSV;
+	use \Exception;
+	/**
+	 * Exception subclass for the PhpCSV_Parser class.
+	 * It contains some additional properties which the base PHP Exception class doesn't contain
+	 *
+	 * Methods (Getters)
+	 * ------------------------------
+	 * getDescription() - Returns the description of the error that was thrown..if set
+	 * getFilename() - Returns the name of the file where the exception was thrown (should always be class.php-csv-parser.php)
+	 * getLinenumber() - Returns the line within the file where the error condition occurred
+	 * getFullMessage() - Returns a full error response string mixing together all the properties of the class
+	 *
+	 * @author Emmanuel
+	 * @version 1.0.0
+	 * @package Emmanuel/php_csv_parser/
+	 * @link
+	 */
+	class PhpCSV_Parser_Exception extends Exception {
+		private $error_description;
+		private $error_linenum;
+		private $error_filename;
+		/**
+		 * The class constructor for this Exception subclass
+		 * @param string $message The main error message
+		 * @param string $filename The name of the file where the error occurred
+		 * @param string $linenum The line on which the error condition occurred
+		 * @param string $description A longer description of the error and probable recommendations
+		 */
+		public function __construct($message,$filename,$linenum,$description) {
+			parent::__construct($message, null, null);
+			$this->setFilename($filename)
+				->setDescription($description)
+				->setLineNumber($linenum);
+		}
+		/**
+		 * Returns the error description
+		 * @return string
+		 */
+		public function getDescription(){
+			return ($this->error_description)? $this->error_description:"";
+		}
+		/**
+		 * Returns the name of the file where the exception was thrown
+		 * @return string
+		 */
+		public function getFilename(){
+			return ($this->error_filename)? $this->error_filename:"";
+		}
+		/**
+		 * Returns the line number where the error was triggered in the file
+		 * @return int
+		 */
+		public function getLineNumber(){
+			return ($this->error_linenum)? $this->error_linenum:"";
+		}
+		/**
+		 * Returns the full error messgae taking into consideration the context of the error
+		 * @return string
+		 */
+		public function getFullMessage(){
+			$error_message = sprintf(
+				"Error with message '%s (Description: %s)' in file '%s' on line %d",
+				parent::getMessage(), $this->getDescription(), $this->getFilename(), $this->getLineNumber()
+			);
+			return $error_message;
+		}
+		/**
+		 * Sets the value for the description property (i.e. the description of the exception that was thrown)
+		 * @param string $description
+		 * @return \PhpCSV\PhpCSV_Parser_Exception
+		 */
+		public function setDescription($description=null){
+			if(is_string($description) and !empty($description)){
+				$this->error_description = trim($description);
+			}
+			return $this;
+		}
+		/**
+		 * Sets the value for the filename property (i.e. the filename where the excetion was thrown)
+		 * @param string $filename
+		 * @return \PhpCSV\PhpCSV_Parser_Exception
+		 */
+		public function setFilename($filename=null){
+			if(is_string($filename) and !empty($filename)){
+				$this->error_filename = trim(error_filename);
+			}
+			return $this;
+		}
+		/**
+		 * Sets the value for the line number property (i.e. the line where the excetion was thrown)
+		 * @param int $linenum
+		 * @return \PhpCSV\PhpCSV_Parser_Exception
+		 */
+		public function setLineNumber($linenum=null){
+			if(is_numeric($linenum)){
+				$this->error_linenum = trim($linenum);
+			}
+			return $this;
+		}
+	}
+?>
--- class.php-csv-parser.php
+++ class.php-csv-parser.php
@@ -1,8 +1,11 @@
 <?php
 	namespace PhpCSV;
+	$cwd = dirname(__FILE__);
+	require_once($cwd."/class.php-csv-parser-exception.php");
 	/**
 	 * This class is a simple PHP CSV parser inspired by the Nodejs CSV project (see https://github.com/wdavidw/node-csv).
-	 * It presents a lot of the functionality in the NodeJs project for use in everyday CSV processing in PHP
+	 * It presents a lot of the functionality (with some minor changes) in the NodeJs project for use in everyday CSV
+	 * processing in PHP.
 	 * Hope someone finds it useful
 	 *
 	 * @author Okeke Emmanuel <emmanix2002@gmail.com>
@@ -11,8 +14,144 @@
 	 * @link
 	 */
 	class PhpCSV_Parser {
+		/**
+		 * The array to hold the raw data before it's processed
+		 * @var array
+		 */
+		private $data_array;
+		/**
+		 * The array to hold the options for the parser
+		 * @var array
+		 */
+		private $options;
+		/**
+		 * The array to hold the default options for the parser
+		 * @var array
+		 */
+		private $options_default;
 
+		const EVENT_ON_RECORD = "record";
+		const EVENT_ON_DATA = "data";
+		const EVENT_ON_CLOSE = "close";
+		const EVENT_ON_END = "end";
+		const EVENT_ON_ERROR = "error";
+		/**
+		 * The class constructor
+		 */
 		public function __construct(){
+			$this->data_source = null;
+			$this->data_array = array();
+			$this->options_default = array(
+				"comment"=>"#",
+				"delimiter"=>","
+			);
+		}
+		/**
+		 * Handles the function of throwing an exception for this file
+		 * @param string $message
+		 * @param int $linenum
+		 * @param string $description
+		 * @throws PhpCSV_Parser_Exception
+		 */
+		private function throwException($message, $linenum, $description){
+			throw new PhpCSV_Parser_Exception($message,__FILE__,$linenum,$description);
+		}
+		/**
+		 * Sets the options on the parser
+		 * @param array $options The options array support certain settings which are highlighted below
+		 *		<ul>
+		 *			<li><b>comment</b>: a single character that is used to identify lines containing comments (default: #)</li>
+		 *			<li><b>delimiter</b>: a single character that serves as the delimiter for content on each line (default: ,)</li>
+		 *		</ul>
+		 * @return \PhpCSV\PhpCSV_Parser
+		 */
+		public function setOptions(array $options=null){
+			$this->options = array_merge($this->options_default,$options);
+			return $this;
+		}
+		/**
+		 * Sets the CSV data to be processed as a string
+		 * @param string $csv_string
+		 * @param array $options The options array to use for the parsing (see the setOption() method)
+		 * @return \PhpCSV\PhpCSV_Parser
+		 * @see setOptions()
+		 * @throws PhpCSV_Parser_Exception
+		 */
+		public function from_string($csv_string=null, array $options=null){
+			$this->setOptions($options);
+			$error_line = __LINE__ + 1; #WARNING: don't move the line above away from there
+			if($csv_string === null or !is_string($csv_string) or strlen($csv_string) === 0){
+				$this->throwException(
+					"Argument 1 should be a non-empty string",
+					$error_line,
+					"Arguments 1 must be a string"
+				);
+			}
+			$lines = explode(PHP_EOL, $csv_string);
+			$this->setDataArray($lines);
+			return $this;
+		}
+		/**
+		 * Takes an array of strings and sets it up for parsing
+		 * @param array $array The array of strings to be processed
+		 * @param array $options
+		 * @return \PhpCSV\PhpCSV_Parser
+		 */
+		public function from_array(array $array, array $options=null){
+			$this->setOptions($options);
+			$error_line = __LINE__ + 1; #WARNING: don't move the line above away from there
+			if(empty($array)){
+				$this->throwException(
+					"Argument 1 should be a non-empty array",
+					$error_line,
+					"Arguments 1 must be an array that contains at least one value"
+				);
+			}
+			$this->setDataArray($array);
+			return $this;
+		}
+		/**
+		 * Takes a filepath to a CSV file and uses it as the input source for the parser
+		 * @param string $csv_filepath
+		 * @param array $options
+		 * @return \PhpCSV\PhpCSV_Parser
+		 * @throws PhpCSV_Parser_Exception
+		 */
+		public function from_path($csv_filepath=null, array $options=null){
+			$this->setOptions($options);
+			$error_line = __LINE__ + 1; #WARNING: don't move the line above away from there
+			if($csv_filepath === null or !is_string($csv_filepath) or strlen($csv_filepath) === 0){
+				$this->throwException(
+					"Argument 1 should be a non-empty string",
+					$error_line,
+					"Arguments 1 must be a string"
+				);
+			}
+			$file_handle = @fopen($csv_filepath, "rt");
+			return $this;
+		}
+
+		public function from_stream(&$file_handle, array $options=null){
+			$this->setOptions($options);
+			$error_line = __LINE__ + 1; #WARNING: don't move the line above away from there
+			return $this;
+		}
+		/**
+		 * Sets the data array with the content that will be processed at a later time
+		 * @param array $lines An array containing the raw processed data
+		 * @return \PhpCSV\PhpCSV_Parser
+		 */
+		private function setDataArray(array $lines){
+			foreach($lines as $line){
+				if(substr($line, 0, 1) !== $this->options['comment']){
+					#not a comment line
+					$this->data_array[] = $line;
+				}
+			}
+			return $this;
+		}
+
+		public function on($event_name, $callable){
 
 		}
 
