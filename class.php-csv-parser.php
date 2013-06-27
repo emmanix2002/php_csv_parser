0cfcf6a3719414427e4fa39baabe60df5ead2609
okeke emmanuel <emmanix2002@gmail.com>
2013-06-27 11:08:56 +0100

    Added some inline documentation
    also added the constructor. Also decided against using just CSV for the
    namespace

--- class.php-csv-parser.php
+++ class.php-csv-parser.php
@@ -1,7 +1,9 @@
 <?php
-	namespace CSV;
+	namespace PhpCSV;
 	/**
-	 * Description of class
+	 * This class is a simple PHP CSV parser inspired by the Nodejs CSV project (see https://github.com/wdavidw/node-csv).
+	 * It presents a lot of the functionality in the NodeJs project for use in everyday CSV processing in PHP
+	 * Hope someone finds it useful
 	 *
 	 * @author Okeke Emmanuel <emmanix2002@gmail.com>
 	 * @version 1.0.0
@@ -9,6 +11,10 @@
 	 * @link
 	 */
 	class PhpCSV_Parser {
+
+		public function __construct(){
+
+		}
 
 	}
 ?>
